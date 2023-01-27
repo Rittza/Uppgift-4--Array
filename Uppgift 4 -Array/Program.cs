4949c5a3cc91dda485ee0d09746dbcbfa99e5632
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-01-27 10:46:36 +0100

    Add project files.

---
+++ Uppgift 4 -Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift_4_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = { "Elijah", "Tim", "Eric", "Simon", "Ameen" };
+            string förstanamnet = namn[0];
+
+            for(int i = 1; i < namn.Length; i++)
+            {
+                if (namn[i].CompareTo(förstanamnet) < 0)
+                {
+                    Console.WriteLine(namn[i] +  " Kommer först ");
+                }
+            }
+        }
+    }
+}
